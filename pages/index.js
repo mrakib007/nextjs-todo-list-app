882b2fa63588d0c373f9086d1471b41729e65675
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-05-07 12:27:25 +0600

    Working on the backend

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [todo,setTodo] = useState({title:"",desc:""});
+
+  const addTodo = () => {
+    let todos = localStorage.getItem("todos");
+    if(todos){
+      let todosJson = JSON.parse(todos);
+      if(todosJson.filter(value=>{return value.title == todo.title}).length > 0){
+        alert("This Entry Already Exists!");
+      }else{
+        todosJson.push(todo);
+        localStorage.setItem("todos",JSON.stringify(todosJson));
+        alert("Todo Successfully Added!");
+        setTodo({title:"",desc:""})
+      }
+    }else{
+      localStorage.setItem("todos",JSON.stringify([todo]))
+    }
+  }
+  const onChange = (e) =>{
+    setTodo({...todo,[e.target.name]:e.target.value});
+    console.log(todo)
+  }
   return (
     <div className="my-2 text-3xl">
       <section className="text-gray-600 body-font">
@@ -15,30 +38,34 @@ export default function Home() {
             </h2>
             <div className="relative mb-4">
               <label
-                for="full-name"
+                for="title"
                 className="leading-7 text-sm text-gray-600"
               >
               Todo Title
               </label>
               <input
+              onChange={onChange}
+              value={todo.title}
                 type="text"
-                id="full-name"
-                name="full-name"
+                id="title"
+                name="title"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <div className="relative mb-4">
-              <label for="email" className="leading-7 text-sm text-gray-600">
+              <label for="desc" className="leading-7 text-sm text-gray-600">
                 Todo Text
               </label>
               <input
-                type="email"
-                id="email"
-                name="email"
+              onChange={onChange}
+              value={todo.desc}
+                type="text"
+                id="desc"
+                name="desc"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
-            <button className="text-white bg-indigo-500 w-fit border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <button onClick={addTodo} className="text-white bg-indigo-500 w-fit border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
               Add A Todo
             </button>
             <p className="text-xs text-gray-500 mt-3">
